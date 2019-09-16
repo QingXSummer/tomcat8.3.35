0e14e6a9f1fcb22af093308c105bdc63dee02ed4
QingX <xiaqing@bonc.com.cn>
2019-09-16 21:32:39 +0800

    新增

---
+++ java/com/bonc/Test.java
@@ -0,0 +1,17 @@
+package com.bonc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 描述    :
+ * Author :Qing_X
+ * Date   :2019-04-20 11:21
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
